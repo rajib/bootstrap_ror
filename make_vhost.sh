fecc7afb3ea43bca65f14d56f6b146f995788e30
Rajib Chowdhury <chowdhury284@gmail.com>
2018-07-02 14:32:51 +0530

    Update make_vhost.sh

--- make_vhost.sh
+++ make_vhost.sh
@@ -4,24 +4,25 @@ PassengerRuby /home/ubuntu/.rvm/wrappers/ruby-1.9.3-p194/ruby" >> /etc/apache2/a
 
 
 echo "<VirtualHost *:80>
-        ServerName linksnse.indusnetlabs.com
-        DocumentRoot /home/ubuntu/RailsApps/linksnse/current/public
-	RailsEnv live
+        ServerName example.com
+	ServerAlias www.example.com
+        DocumentRoot /home/ubuntu/RailsApps/example/current/public
+	RailsEnv production
         RewriteEngine On
         ErrorDocument 503 /system/maintenance.html
         RewriteCond %{REQUEST_URI} !.(css|gif|jpg|png)$
         RewriteCond %{DOCUMENT_ROOT}/system/maintenance.html -f
         RewriteCond %{SCRIPT_FILENAME} !maintenance.html
         RewriteRule ^.*$ - [L,R=503]
-        <Directory /home/ubuntu/RailsApps/linksnse/current/public>
+        <Directory /home/ubuntu/RailsApps/example/current/public>
                 AllowOverride all
                 Options -MultiViews
                 Allow from all
                 Require all granted
         </Directory>
-</VirtualHost>" > /etc/apache2/sites-available/linksnse
+</VirtualHost>" > /etc/apache2/sites-available/example
 
-sudo a2ensite linksnse 
+sudo a2ensite example 
 sudo a2enmod rewrite
 sudo a2dissite default
 sudo service apache2 reload
